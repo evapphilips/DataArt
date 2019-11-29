b8551836300a57c8e42c20d6c4b0b22583c1e349
evapphilips <ep1521@nyu.edu>
2019-11-29 17:40:09 -0500

    adding complaint data script

---
+++ finalProject/retrieveDataTest/parseData.js
@@ -0,0 +1,89 @@
+// when the page first loads
+window.addEventListener("load", init);
+function init(){
+    
+    // initiate voice 
+    var myVoice = new p5.Speech(); // new P5.Speech object
+    
+    // store API info
+    var complaintURL = "https://data.cityofnewyork.us/resource/erm2-nwe9.json?borough="
+    var openDataToken = "i8FbjVS1B0bIfBbl3QQhFR0wf" // * INSERT API TOKEN * //
+    
+    // variables
+    var borough = "BROOKLYN" // set defailt borough to brooklyn
+    var boroughChoices = ["MANHATTAN", "BROOKLYN", "QUEENS", "BRONX", "STATEN%20ISLAND"]
+
+    // create API url
+    var dataURL = complaintURL + borough + "&$$app_token=" + openDataToken;
+    //console.log(dataURL)
+
+    // show default complaints when the page loads
+    updateComplaints()
+
+    // access dropdown and add a event listener to it
+    var boroughDropdown = document.getElementById("boroughOption");
+    boroughDropdown.addEventListener("change", changeBorough);
+    //when you select a new borough, update the complaints
+    function changeBorough(){
+        // stop reading
+        //myVoice.stop();
+
+        //remove existing complaints
+        for(let i=0; i<1000; i++){
+            //console.log("removing")
+            var oldComplaint = document.getElementById("theComplaints")
+            // document.body.removeChild(oldComplaint);
+            oldComplaint.remove();
+        }
+
+        // change borough value
+        var index = boroughDropdown.value;
+        borough = boroughChoices[index];
+        console.log(borough)
+
+        //add new complaints
+        updateComplaints();
+        
+
+    }
+    
+
+    // load complaint data 
+    function updateComplaints(){
+        (d3.json(dataURL)).then(data => {
+            // place raw data into object (we only want to use, date, agency, type, descriptor)
+            const complaints = data;
+    
+            // for each complain, create string to print and add it to a concatinated string
+            var totalComplaints = ""
+            complaints.forEach(complaint => {
+                var date = complaint.created_date;
+                var agency = complaint.agency; // or agency_name
+                var type = complaint.complaint_type;
+                var descriptor = complaint.descriptor;
+                //var sent = "On " + date + " a complaint was made to the " + agency + " regarding " + type + " - " + descriptor;
+                var sent = agency + " received a complaint regarding " + type + " - " + descriptor + ".  ";
+                //complaint.toPrint = sent;
+                totalComplaints += sent + ".  ";
+    
+                // create a new paragraph for each complaint
+                var complaintDiv = document.getElementById("complaintDiv")
+                var newComplaint = document.createElement('p');
+                newComplaint.innerHTML = sent;
+                newComplaint.id = "theComplaints";
+                if(agency === "DSNY"){
+                    newComplaint.style.backgroundColor = "#008000";
+                }
+                complaintDiv.appendChild(newComplaint)
+            })
+
+            //start reading
+	        //myVoice.speak(totalComplaints);
+    
+            // access paragraph element
+            // var para = document.getElementById("paragraphComplaints");
+            // para.innerHTML = paraComplaints;   
+        })
+
+    }
+}
